c58fa2411aaef4da726cd27c0768b2ecee3d8b26
Easterok <beskrovnyk@gmail.com>
2024-04-16 05:34:19 +0300

    feat: info route

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/easterok/turbocache/pkg/storage"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+)
+
+type (
+	Meta struct {
+		Hash          string `json:"hash"`
+		ContentLength string `json:"content-length"`
+		Duration      string `json:"duration"`
+	}
+
+	Event struct {
+		SessionId string `json:"sessionId"`
+		Source    string `json:"source"`
+		Event     string `json:"event"`
+		Hash      string `json:"hash"`
+		Duration  int    `json:"duration"`
+	}
 )
 
 func main() {
@@ -65,11 +83,73 @@ func main() {
 		},
 	}))
 
+	artifacts.GET("/info", func(c echo.Context) error {
+		team := c.QueryParam("teamid")
+
+		if team == "" {
+			team = c.QueryParam("slug")
+		}
+
+		if team == "" {
+			return c.String(http.StatusBadRequest, "querystring should have required property teamid or slug")
+		}
+
+		meta, err := storage.GetMeta(team)
+
+		if err != nil {
+			return c.JSON(http.StatusOK, Info{
+				CorruptedData: true,
+			})
+		}
+
+		events, err := storage.GetEvents(team)
+
+		if err != nil {
+			return c.JSON(http.StatusOK, Info{
+				CorruptedData: true,
+			})
+		}
+
+		status := prepareInfo(string(meta), string(events))
+
+		return c.JSON(http.StatusOK, status)
+	})
+
 	artifacts.GET("/status", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "enabled"})
 	})
 
 	artifacts.POST("/events", func(c echo.Context) error {
+		team := c.QueryParam("teamid")
+
+		if team == "" {
+			team = c.QueryParam("slug")
+		}
+
+		if team == "" {
+			return c.String(http.StatusBadRequest, "querystring should have required property teamid or slug")
+		}
+
+		value := []Event{}
+
+		err := json.NewDecoder(c.Request().Body).Decode(&value)
+
+		if err != nil {
+			return c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		}
+
+		m, err := json.Marshal(value)
+
+		if err != nil {
+			return c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		}
+
+		err = storage.SaveEvent(team, m)
+
+		if err != nil {
+			return c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		}
+
 		return c.String(http.StatusOK, "Success. Event recorded.")
 	})
 
@@ -106,10 +186,25 @@ func main() {
 			return c.String(http.StatusBadRequest, "querystring should have required property teamid or slug")
 		}
 
+		duration := c.Request().Header.Get("x-artifact-duration")
+		contentLength := c.Request().Header.Get("Content-Length")
+
 		err := storage.Put(hash, team, c.Request().Body)
 
 		if err != nil {
 			return c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+		}
+
+		meta, _ := json.Marshal(Meta{
+			Duration:      duration,
+			ContentLength: contentLength,
+			Hash:          hash,
+		})
+
+		err = storage.SaveMeta(hash, team, meta)
+
+		if err != nil {
+			fmt.Printf("error saving to meta file %v", err)
 		}
 
 		return c.String(http.StatusAccepted, "ok")
@@ -137,4 +232,84 @@ func main() {
 	})
 
 	log.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("API_PORT"))))
+}
+
+type Info struct {
+	CorruptedData    bool   `json:"corrupted_data"`
+	SavedTime        string `json:"saved_time"`
+	TotalComputeTime string `json:"total_compute_time"`
+	ContentLength    string `json:"content_length"`
+}
+
+func prepareInfo(meta, events string) Info {
+	_meta := []Meta{}
+
+	err := json.Unmarshal([]byte("["+strings.Join(strings.Split(strings.TrimSpace(meta), "\n"), ",")+"]"), &_meta)
+
+	if err != nil {
+		return Info{
+			CorruptedData: true,
+		}
+	}
+
+	_events := [][]Event{}
+
+	err = json.Unmarshal([]byte("["+strings.Join(strings.Split(strings.TrimSpace(events), "\n"), ",")+"]"), &_events)
+
+	if err != nil {
+		return Info{
+			CorruptedData: true,
+		}
+	}
+
+	duration := 0
+	saved := 0
+	contentLength := 0
+
+	for _, item := range _meta {
+		a, _ := strconv.Atoi(item.ContentLength)
+
+		contentLength += a
+	}
+
+	corrupted_data := false
+
+	remote := []Event{}
+
+	for _, i := range _events {
+		for _, event := range i {
+			if event.Source == "REMOTE" {
+				remote = append(remote, event)
+			}
+		}
+	}
+
+	for _, event := range remote {
+		founded := false
+
+		for _, item := range _meta {
+			if !founded && item.Hash == event.Hash {
+				_duration, _ := strconv.Atoi(item.Duration)
+
+				if event.Event == "HIT" {
+					saved += _duration
+				} else {
+					duration += _duration
+				}
+
+				founded = true
+			}
+
+			duration += event.Duration
+		}
+
+		corrupted_data = !founded || corrupted_data
+	}
+
+	return Info{
+		CorruptedData:    corrupted_data,
+		SavedTime:        fmt.Sprintf("%d (ms)", saved),
+		TotalComputeTime: fmt.Sprintf("%d (ms)", duration),
+		ContentLength:    fmt.Sprintf("%d (bytes)", contentLength),
+	}
 }
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -33,14 +33,14 @@ func createFolder(p string) error {
 	return nil
 }
 
-func (s *Disk) Put(hash, team string, reader io.ReadCloser) error {
+func (d *Disk) Put(hash, team string, reader io.ReadCloser) error {
 	b, err := io.ReadAll(reader)
 
 	if err != nil {
 		return err
 	}
 
-	teamfolder := filepath.Join(s.Path, team)
+	teamfolder := filepath.Join(d.Path, team)
 
 	err = createFolder(teamfolder)
 
@@ -53,8 +53,62 @@ func (s *Disk) Put(hash, team string, reader io.ReadCloser) error {
 	return os.WriteFile(p, b, 0644)
 }
 
-func (s *Disk) Get(hash, team string) ([]byte, error) {
-	p := filepath.Join(s.Path, team, hash)
+func (d *Disk) Get(hash, team string) ([]byte, error) {
+	p := filepath.Join(d.Path, team, hash)
+
+	return os.ReadFile(p)
+}
+
+func saveToFile(name string, data []byte) error {
+	file, err := os.OpenFile(name, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = file.Write(append(data, byte('\n')))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Disk) SaveEvent(team string, data []byte) error {
+	teamfolder := filepath.Join(d.Path, team)
+
+	err := createFolder(teamfolder)
+
+	if err != nil {
+		return err
+	}
+
+	return saveToFile(filepath.Join(teamfolder, "_events"), data)
+}
+
+func (d *Disk) SaveMeta(hash, team string, data []byte) error {
+	teamfolder := filepath.Join(d.Path, team)
+
+	err := createFolder(teamfolder)
+
+	if err != nil {
+		return err
+	}
+
+	return saveToFile(filepath.Join(teamfolder, "_meta"), data)
+}
+
+func (d *Disk) GetMeta(team string) ([]byte, error) {
+	p := filepath.Join(d.Path, team, "_meta")
+
+	return os.ReadFile(p)
+}
+
+func (d *Disk) GetEvents(team string) ([]byte, error) {
+	p := filepath.Join(d.Path, team, "_events")
 
 	return os.ReadFile(p)
 }
